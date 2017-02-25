1d2eb369fecb43be797c7008a5f53537cad6466e
Phan Hong An <phanhongan@gmail.com>
2017-02-25 23:12:11 +0700

    update location_history table

--- location-reports-server/database/db.sql
+++ location-reports-server/database/db.sql
@@ -1,14 +1,17 @@
-CREATE TABLE location_history (
-  id  				      SERIAL PRIMARY KEY,
-  uid               VARCHAR(25) NOT NULL,
-  date_index        INT4 NOT NULL,
-  latitude          FLOAT8 NOT NULL,
-  longitude         FLOAT8 NOT NULL,
-  mapping_latitude  FLOAT8 NOT NULL,
-  mapping_longitude FLOAT8 NOT NULL,
-  full_address      VARCHAR(512),
-  street            VARCHAR(256),
-  ward              VARCHAR(128),
-  district          VARCHAR(64),
-  city              VARCHAR(64)
+CREATE EXTENSION postgis;
+
+CREATE SCHEMA location_report;
+
+CREATE TABLE location_report.location_history (
+  id  				  SERIAL PRIMARY KEY,
+  uid           VARCHAR(25) NOT NULL,
+  geom          GEOMETRY NOT NULL,
+  m_geom        GEOMETRY NOT NULL,
+  created_at    TIMESTAMP NOT NULL,
+  date_index    INTEGER NOT NULL,
+  full_address  VARCHAR(512),
+  street        VARCHAR(256),
+  ward          VARCHAR(128),
+  district      VARCHAR(64),
+  city          VARCHAR(64)
 );
