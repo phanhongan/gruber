9f4e3110a17ace3ffa910d45b6019983da21b9b8
Phan Hong An <phanhongan@gmail.com>
2017-02-26 14:59:59 +0700

    add place_id

--- location-reports-server/database/db.sql
+++ location-reports-server/database/db.sql
@@ -4,9 +4,10 @@ CREATE SCHEMA location_report;
 
 CREATE TABLE location_report.location_history (
   id  				  SERIAL PRIMARY KEY,
-  uid           VARCHAR(25) NOT NULL,
+  uid           VARCHAR(32) NOT NULL,
   geom          GEOMETRY NOT NULL,
-  m_geom        GEOMETRY NOT NULL,
+  place_id      VARCHAR(32) NOT NULL,
+  place_geom    GEOMETRY NOT NULL,
   created_at    TIMESTAMP NOT NULL,
   date_index    INTEGER NOT NULL,
   full_address  VARCHAR(512),
